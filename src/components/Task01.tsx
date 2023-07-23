f9e8e7fbe380c3fd6c06959b8dfd8e7838ca5289
blacknii <42908977+blacknii@users.noreply.github.com>
2023-07-23 15:34:13 +0200

    .

--- src/components/Task01.tsx
+++ src/components/Task01.tsx
@@ -6,20 +6,11 @@ const getUpper = (val: string) => {
   return val.toUpperCase();
 };
 
-const singUpUser = (
-  name: string,
-  email: string,
-  isPaid: boolean = false
-): string => {
-  return `name: ${name} - email: ${email} -  ${isPaid ? "paid" : "not paid"}`;
-};
-
 const Task01 = () => {
   return (
     <div>
       <p>{addTwo(2)}</p>
       <p>{getUpper("hello")}</p>
-      <p>{singUpUser("John", "john@gmail.com", true)}</p>
     </div>
   );
 };
