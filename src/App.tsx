91bf0120304532d689a4dd67f29a036db8ce4175
blacknii <42908977+blacknii@users.noreply.github.com>
2023-07-24 10:57:41 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import "./App.css";
+import Task04 from "./components/Task04";
 // import Task01 from "./components/Task01";
 // import Task02 from "./components/Task02";
-import Task03 from "./components/Task03";
+// import Task03 from "./components/Task03";
 
 function App() {
   return (
     <>
       {/* <Task01 />
       <Task02 /> */}
-      <Task03 />
+      {/* <Task03 /> */}
+      <Task04 />
     </>
   );
 }
