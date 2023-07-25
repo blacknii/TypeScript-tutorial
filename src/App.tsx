fb9441cb00eb25912e8badfa4040130d7b4da4a2
blacknii <42908977+blacknii@users.noreply.github.com>
2023-07-25 18:27:03 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import "./App.css";
-// import Task05 from "./components/Task05";
-// import Task01 from "./components/Task01";
-// import Task02 from "./components/Task02";
-// import Task03 from "./components/Task03";
+import Heading from "./components/Heading";
+import { Section } from "./components/Section";
 
 function App() {
   return (
     <>
-      {/* <Task01 /> */}
-      {/* <Task02 /> */}
-      {/* <Task03 /> */}
-      {/* <Task04 /> */}
+      <Heading title="Hello" />
+      <Section title={"Different Title"}>This is my Section.</Section>
     </>
   );
 }
